f5b5eceaaec657f55102a19464e0d2f04235d5f1
soursarkar1990 <souravsarkar1990@gmail.com>
2023-07-25 18:51:47 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,19 +1,7 @@
 // create a web server and listen to incoming request
+
 // 1. require http module
-const http = require('http');
-// 2. create a server
-const server = http.createServer((req, res) => {
-    if (req.url === '/') {
-        res.write('Hello World');
-        res.end();
-    }
-    if (req.url === '/api/comments') {
-        res.write(JSON.stringify([1, 2, 3]));
-        res.end();
-    }
-});
-// 3. listen to incoming request
-server.listen(3000);
+
 console.log('Listening on port 3000...');
 
 // Path: comments.js
