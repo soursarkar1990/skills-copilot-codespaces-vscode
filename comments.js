0230636f824be82cb2f3dbab3333725ff83a27dd
soursarkar1990 <souravsarkar1990@gmail.com>
2023-07-25 18:49:04 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,63 +1,20 @@
-// create a web server
-// 1. require express
-const express = require('express');
-// 2. create an express app
-const app = express();
+// create a web server and listen to incoming request
+// 1. require http module
+const http = require('http');
+// 2. create a server
+const server = http.createServer((req, res) => {
+    if (req.url === '/') {
+        res.write('Hello World');
+        res.end();
+    }
+    if (req.url === '/api/comments') {
+        res.write(JSON.stringify([1, 2, 3]));
+        res.end();
+    }
+});
 // 3. listen to incoming request
-app.listen(3000, () => console.log('Listening on port 3000...'));
-// 4. create a route
-app.get('/', (req, res) => {
-    res.send('Hello World');
-});
-// 5. create a route
-app.get('/api/comments', (req, res) => {
-    res.send([1, 2, 3]);
-});
-// 6. create a route
-app.get('/api/comments/:id', (req, res) => {
-    res.send(req.params.id);
-});
-// 7. create a route
-app.get('/api/comments/:year/:month', (req, res) => {
-    res.send(req.params);
-});
-// 8. create a route
-app.get('/api/comments/:year/:month', (req, res) => {
-    res.send(req.query);
-});
-// 9. create a route
-app.get('/api/comments/:year/:month', (req, res) => {
-    res.send(req.query);
-});
-// 10. create a route
-app.get('/api/comments/:year/:month', (req, res) => {
-    res.send(req.query);
-});
-// 11. create a route
-app.get('/api/comments/:year/:month', (req, res) => {
-    res.send(req.query);
-});
-// 12. create a route
-app.get('/api/comments/:year/:month', (req, res) => {
-    res.send(req.query);
-});
-// 13. create a route
-app.get('/api/comments/:year/:month', (req, res) => {
-    res.send(req.query);
-});
-// 14. create a route
-app.get('/api/comments/:year/:month', (req, res) => {
-    res.send(req.query);
-});
-// 15. create a route
-app.get('/api/comments/:year/:month', (req, res) => {
-    res.send(req.query);
-});
-// 16. create a route
-app.get('/api/comments/:year/:month', (req, res) => {
-    res.send(req.query);
-});
-// 17. create a route
-app.get('/api/comments/:year/:month', (req, res) => {
-    res.send(req.query);
-});
+server.listen(3000);
+console.log('Listening on port 3000...');
+
+// Path: comments.js
+// create a web server and listen to incoming request
